77b77c0e6b2623f20708ddcce1a9b67e007668b7
zenmarev@gmail.com <zenmarev@gmail.com>
2021-02-03 21:46:39 -0500

    init

---
+++ src/LaneGG/LaneGG.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LaneGG.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
